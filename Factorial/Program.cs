36d7f41b2882f2d26ca202dbb675e8e3eb85b45e
amirreza tavakoli <amirrezatav2@gmail.com>
2022-02-17 00:57:52 +0330

    - add Factorial code

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace Factorial
 {
-    internal class Program
+    internal class Factorial
     {
         static void Main(string[] args)
         {
+            int num = 0;
+            int fact = 1;
+            string input = Console.ReadLine(); // Input
+            num = Convert.ToInt32(num); // Convert string to normal int 
+
+            for (int i = 2; i <= num; i++)
+                fact *= i; // n! = n * (n-1)! 
+
+            Console.Write("Fact = "); // Output without Enter(\n)
+            Console.WriteLine(fact); // Output with Enter(\n)
+
+            Console.ReadLine(); // To prevent console closure
         }
     }
 }
